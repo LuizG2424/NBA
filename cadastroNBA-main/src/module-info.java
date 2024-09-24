abd2a19ea26203375a293320a41792ab669b8d2a
lab04 <lab04@lab4-10>
2024-09-23 22:52:12 -0300

    Classes Conexao e OperacaoBanco criadas

--- cadastroNBA-main/src/module-info.java
+++ cadastroNBA-main/src/module-info.java
@@ -2,6 +2,8 @@ module LAAnalytics {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires javafx.graphics;
+	requires java.sql;
+	requires javafx.base;
 	
 	opens usa.com.nba.atletas to javafx.graphics, javafx.fxml;
 }
